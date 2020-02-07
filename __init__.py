3529ad92c622cd905a78a611076fce1ee8587672
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-02-07 18:07:15 +0800

    Added init files for easy import

---
+++ __init__.py
@@ -0,0 +1,9 @@
+
+# move object access top directory
+from bILT.src.ilt import ilt
+from bILT.src.bILT import bILT
+
+__all__ = ['bILT','ilt']
+__version__ = '0.0.0'
+__author__ = 'Derek Fujimoto, Ryan McFadden'
+
