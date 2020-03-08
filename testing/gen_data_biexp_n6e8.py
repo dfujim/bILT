e51a8c38562946fb0528a7bdb8e65ad93b3e8656
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-03-08 15:08:58 -0700

    Fixed issue in biexp data gen on 1/T1 vs T1

--- testing/gen_data_biexp_n6e8.py
+++ testing/gen_data_biexp_n6e8.py
@@ -19,11 +19,11 @@ d = data_iterator(output_dir)
 d.n = 6e8
 
 # set up variable biexponential relaxation 
-fn = '0.35 * (exp(-x) + exp(-%f*x))'
+fn = '0.35 * (exp(-x) + exp(-x/%f))'
 T1b = np.logspace(-1,2,25)
 
 for i in T1b: 
     d.filename = (filename % float(i)).replace('.','p')
     d.fn = fn%i
-    d.inputs_to_save = {'T1a (1/s)':1,'T1b (1/s)':float(i)}
+    d.inputs_to_save = {'T1a (s)':1,'T1b (s)':float(i)}
     d.run()
