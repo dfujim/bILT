f15c41008d76b4e9d0d0335ae62db9b551ccd5cb
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-04-08 17:03:31 -0700

    comapre curve plots: compare all four

--- testing/data/biexp_n6e8/BestAlpha_balanced/compare_curvature_alpha.py
+++ testing/data/biexp_n6e8/BestAlpha_balanced/compare_curvature_alpha.py
@@ -12,12 +12,14 @@ from bILT.testing.ilt4sim import ilt4sim
 
 # load files 
 dfb = pd.read_csv('best_alpha.csv',comment='#')
-dfc = pd.read_csv('../BestAlpha/best_alpha.csv',comment='#')
+dfc = pd.read_csv('../BestAlpha_curvature/best_alpha.csv',comment='#')
 
 # draw to compare 
 plt.figure()
-plt.semilogx(dfc['T1b'],dfc['lcurve'],'-o',label='Curvature Method')
-plt.semilogx(dfb['T1b'],dfb['lcurve'],'-o',label='Balanced Method')
+plt.semilogx(dfc['T1b'],dfc['lcurve'],'-oC0',label='L-curve$_{curvature}$')
+plt.semilogx(dfb['T1b'],dfb['lcurve'],'-oC0',mfc='none',label='L-curve$_{balanced}$')
+plt.semilogx(dfc['T1b'],dfc['scurve'],'-oC1',label='S-curve')
+plt.semilogx(dfc['T1b'],dfc['gcv'],'-oC2',label='GCV')
 plt.xlabel(r'$T_1^{(b)}$ (s)')
 plt.ylabel(r'$\alpha_{opt}$')
 plt.title(r'Bi-Exp with $T_1^{(a)}=1$')
--- testing/data/biexp_n6e8/BestAlpha_balanced/compare_curvature_rchi2.py
+++ testing/data/biexp_n6e8/BestAlpha_balanced/compare_curvature_rchi2.py
@@ -13,23 +13,29 @@ from bILT.testing.ilt4sim import ilt4sim
 
 # load files 
 dfb = pd.read_csv('best_alpha.csv',comment='#')
-dfc = pd.read_csv('../BestAlpha/best_alpha.csv',comment='#')
+dfc = pd.read_csv('../BestAlpha_curvature/best_alpha.csv',comment='#')
 
 # get rchi2 of each 
-def get_rchi2(df):
+def get_rchi2(df,col):
     rchi2 = []
     for i in df.index:
         I = ilt4sim(os.path.join('..',df.loc[i,'filename']))
-        rchi2.append(I.get_rchi2(df.loc[i,'lcurve']))
+        rchi2.append(I.get_rchi2(df.loc[i,col]))
     return rchi2
 
-rchi2b = get_rchi2(dfb)
-rchi2c = get_rchi2(dfc)
+rchi2b = get_rchi2(dfb,'lcurve')
+rchi2c = get_rchi2(dfc,'lcurve')
+rchi2s = get_rchi2(dfc,'scurve')
+rchi2g = get_rchi2(dfc,'gcv')
 
 # draw to compare 
 plt.figure()
-plt.semilogx(dfc['T1b'],rchi2c,'-o',label='Curvature Method')
-plt.semilogx(dfb['T1b'],rchi2b,'-o',label='Balanced Method')
+
+plt.semilogx(dfc['T1b'],rchi2c,'-oC0',label='L-curve$_{curvature}$')
+plt.semilogx(dfb['T1b'],rchi2b,'-oC0',mfc='none',label='L-curve$_{balanced}$')
+plt.semilogx(dfc['T1b'],rchi2s,'-oC1',label='S-curve')
+plt.semilogx(dfc['T1b'],rchi2g,'-oC2',label='GCV')
+
 plt.xlabel(r'$T_1^{(b)}$ (s)')
 plt.ylabel(r'$\chi^2/N$')
 plt.title(r'Bi-Exp with $T_1^{(a)}=1$')
